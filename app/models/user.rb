bede39854ac522d8b4a87fd5ee2927835b878fbf
Ben <asdfzxcvb12371@gmail.com>
2018-04-24 22:30:14 +0800

    setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, sources: :restaurant
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar , AvatarImageUploader
